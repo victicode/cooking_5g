bdecc7e1cd3fa26d8023593a0755557279927c0c
Victor Rivero <VRIVERO@forum.local>
2024-04-11 16:25:27 -0400

    avance en qr y etiquietas2

--- resources/views/tagRecipeFormat.blade.php
+++ resources/views/tagRecipeFormat.blade.php
@@ -88,6 +88,6 @@
         <script src="https://cdn.datatables.net/plug-ins/1.13.4/sorting/date-uk.js"></script>  -->
         <!-- @vite('resources/js/app.js') -->
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     </body>
 </html>
