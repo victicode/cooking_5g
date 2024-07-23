2e122bb9667e2ff7d43ac668b62c606306fa92b3
Victor Rivero <VRIVERO@forum.local>
2024-07-22 21:13:31 -0400

    avances cortos

--- remotex/index.php
+++ remotex/index.php
@@ -21,40 +21,22 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-require __DIR__.'/vendor/autoload.php';
+try {
+    require __DIR__.'/vendor/autoload.php';
 
-/*
-|--------------------------------------------------------------------------
-| Turn On The Lights
-|--------------------------------------------------------------------------
-|
-| We need to illuminate PHP development, so let us turn on the lights.
-| This bootstraps the framework and gets it ready for use, then it
-| will load up this application so that we can run it and send
-| the responses back to the browser and delight our users.
-|
-*/
+    $app = require_once __DIR__.'/bootstrap/app.php';
 
-$app = require_once __DIR__.'/bootstrap/app.php';
+    $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request
-| through the kernel, and send the associated response back to
-| the client's browser allowing them to enjoy the creative
-| and wonderful application we have prepared for them.
-|
-*/
+    $response = $kernel->handle(
+        $request = Illuminate\Http\Request::capture()
+    );
 
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+    $response->send();
 
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
+    $kernel->terminate($request, $response);
+} catch (\Throwable $th) {
+    //throw $th;
+    var_dump($th);
+}
 
-$response->send();
-
-$kernel->terminate($request, $response);
